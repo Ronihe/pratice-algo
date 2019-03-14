e4d90438ee8135b056909ab52083cd0b3026593e
Ronihe <herongrong2011@gmail.com>
2019-03-13 18:44:45 -0700

    started areIdentical

--- binary-search-tree-are-two-trees-identical/index.js
+++ binary-search-tree-are-two-trees-identical/index.js
@@ -6,7 +6,21 @@ const { BinarySearchTree } = require('../_starters/binary-search-tree');
  * @return {boolean}
  */
 
-BinarySearchTree.areIdentical = function areIdentical(rootOne, rootTwo) {};
+BinarySearchTree.areIdentical = function areIdentical(rootOne, rootTwo) {
+  // check the left and right node
+  
+  // create a  vairable to check rootOne and rootTwo has the same left and right node
+  // check the node first, 
+  // if it is true, keep checking their left and right, treat them as the new rootOne and rootTwo
+  //if not match return false
+  let checker = true;
+  
+  
+  
+  
+  
+  
+};
 
 module.exports = {
   BinarySearchTree
